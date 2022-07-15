44d935887cebabc031c9c48a111ac6f453d1ce4b
Fynn Kiwitt <FynnKiwitt@yahoo.de>
2022-07-15 11:05:15 +0200

    name

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,8 +11,10 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "MyTeam";
+        } else if (query.contains("your name")) {
+           return "Fynn";
+        } else if (query.contains("app name")) {
+            return "basicwebapp-42";
         } else { // TODO extend the programm here
             return "";
         }
